6c4df96e84425f455c95e490adf1ad6a2a56bd86
elfcodes808 <teddybear201214@gmail.com>
2025-01-13 18:54:22 -0400

    Update admincommands.lua

--- admincommands.lua
+++ admincommands.lua
@@ -7,23 +7,46 @@ local t1 = lib:NewTab({Name = "Catching"})
 -- Adding a Section
 local s1 = t1:NewSection({Name = "Section"})
 
--- Adding a Toggle
+-- Adding a Toggle for Infinite Jump
 s1:NewToggle({
-    Name = "Magnets",
+    Name = "Inf Jump",  -- Changed name to Inf Jump
     default = false,
     callback = function(v)
-        print("Magnets:", v)
+        if v then
+            -- Enable infinite jump
+            local user = game.Players.LocalPlayer
+            local humanoid = user.Character and user.Character:WaitForChild("Humanoid")
+            if humanoid then
+                humanoid.JumpHeight = 100  -- Increased jump height
+                humanoid.PlatformStand = true  -- Disables platform standing to prevent falling
+                game:GetService("UserInputService").InputBegan:Connect(function(input)
+                    if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.Space then
+                        humanoid:ChangeState(Enum.HumanoidStateType.Physics)
+                    end
+                end)
+            end
+        else
+            -- Disable infinite jump
+            local user = game.Players.LocalPlayer
+            local humanoid = user.Character and user.Character:WaitForChild("Humanoid")
+            if humanoid then
+                humanoid.JumpHeight = 50  -- Reset to default jump height
+                humanoid.PlatformStand = false  -- Enable platform standing
+            end
+        end
     end,
 })
 
--- Adding a Slider
+-- Adding a Slider for Walk Speed
 t1:NewSlider({
-    Name = "Magnets Range",
-    default = 50,
-    min = 0,
-    max = 100,
+    Name = "Walk Speed",  -- Changed name to Walk Speed
+    default = 16,  -- Default value for walk speed (default is 16 in Roblox)
+    min = 0,  -- Min value for walk speed
+    max = 100,  -- Max value for walk speed
     callback = function(v)
-        print("Magnets Range:", v)
+        -- Update the player's walk speed based on slider value
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+        print("Walk Speed:", v)
     end,
 })
 
